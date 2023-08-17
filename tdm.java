585f778eb49691459036e3fcb999849105ed6d01
Find me if you can <140234852+find-mee@users.noreply.github.com>
2023-08-18 02:37:14 +0530

    Create tdm.java

---
+++ tdm.java
@@ -0,0 +1,59 @@
+import java.util.Scanner; 
+public class TDM 
+{ 
+public static void main(String args[]) 
+{ 
+int n,i,qt,count=0,temp,sq=0,bt[],wt[],tat[],rem_bt[]; 
+float awt=0,atat=0; 
+bt = new int[10]; 
+wt = new int[10]; 
+tat = new int[10]; 
+rem_bt = new int[10]; 
+Scanner s=new Scanner(System.in); 
+System.out.print("Enter the number of stations (maximum 10) = "); 
+n = s.nextInt(); 
+System.out.print("Enter the processing time for each channel\n"); 
+for (i=0;i<n;i++) 
+{ 
+System.out.print("S"+i+" = "); //stations Input
+bt[i] = s.nextInt(); 
+rem_bt[i] = bt[i]; 
+} 
+System.out.print("Enter the frame size: "); // Frame size for each station
+qt = s.nextInt(); 
+while(true) 
+{ 
+for (i=0,count=0;i<n;i++) 
+{ 
+temp = qt; 
+if(rem_bt[i] == 0) 
+{ 
+count++; 
+continue; 
+} 
+if(rem_bt[i]>qt) 
+rem_bt[i]= rem_bt[i] - qt; 
+else 
+if(rem_bt[i]>=0) 
+{ 
+temp = rem_bt[i]; 
+rem_bt[i] = 0; 
+} 
+sq = sq + temp; 
+tat[i] = sq; 
+} 
+if(n == count) 
+break; 
+} 
+System.out.print("--------------------------------------------------------------------------------"); 
+System.out.print("\nStation\t Processing Time\t Completion Time\t Waiting Time\n"); 
+System.out.print("--------------------------------------------------------------------------------"); 
+for(i=0;i<n;i++) 
+{ 
+wt[i]=tat[i]-bt[i]; 
+awt=awt+wt[i]; 
+atat=atat+tat[i]; 
+System.out.print("\n \t"+(i+1)+"\t \t"+bt[i]+"\t\t "+tat[i]+"\t\t\t "+wt[i]+"\n"); 
+} 
+} 
+}
